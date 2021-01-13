3fad575e96509d7eb8eef85f2ed5889b5b3f8cf9
Miller Correa <macorreag@unal.edu.co>
2021-01-13 17:11:15 -0500

    First Commit

---
+++ readme.md
@@ -0,0 +1,15 @@
+# Hello Elixir
+
+## How to run
+
+```bash
+    elixir simple.exs
+```
+
+## Learning
+
+- Pattern Matching
+
+```bash
+    elixir simple.exs
+```
---
+++ simple.exs
@@ -0,0 +1,14 @@
+# This is Hello World
+IO.puts "Hello world from Elixir"
+
+# This is a reverse String Function
+myName = String.reverse("Miller")
+IO.puts myName
+
+
+# For obtain help use 👉 "h" example:
+# h(String.reverse())
+# This provide help for the function "String.reverse()"
+
+
+# You can personalize the Interactive Elixir in .iex.exs
