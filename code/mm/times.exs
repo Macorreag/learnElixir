2d8e9082e61824e7093ede19bc125aa8b167200f
Miller Correa <macorreag@unal.edu.co>
2021-01-18 16:32:52 -0500

    modules and named functions

--- code/mm/times.exs
+++ code/mm/times.exs
@@ -10,4 +10,10 @@ defmodule Times do
   def double(n) do
     n * 2
   end
+  def triple(n) do
+    n * 3
+  end
+  def quadruple(n) do
+    double(n) * 2
+  end
 end
--- simple.exs
+++ simple.exs
@@ -122,5 +122,4 @@ IO.puts ("cat" =~ match_end.("!"))
 
 ##
 IO.inspect( Enum.map [1,2,3,4] , &(&1+2) )
-Enum.map [1,2,3,4] , &(IO.inspect(&1))
-Enum.map [1,2,3,4] , fn x -> IO.inspect(x) end
+Enum.map [1,2,3,4] , &(IO.inspect(&1))  # Enum.map [1,2,3,4] , fn x -> IO.inspect(x) end
